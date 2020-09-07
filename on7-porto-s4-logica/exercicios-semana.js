04cdf83ff9f349dc9d0849cf2bd3c89df9b61ca9
elvasthaisa <thaisaelvas@gmail.com>
2020-09-07 17:44:27 -0300

    Finalização da lista de desafios

--- on7-porto-s4-logica/exercicios-semana.js
+++ on7-porto-s4-logica/exercicios-semana.js
@@ -9,7 +9,8 @@
 // 2. Escreva os números na ordem inversa;
 // 3. Multiplique os dígitos das casas ímpares por 2 e subtraia 9 de todos os resultados maiores que 9;
 // 4. Some todos os números;
-// 5. O dzígito verificador (aquele do passo 1) é o número que você precisa somar a todos os outros números somados para fazer a validação;
+// 5. O dígito verificador (aquele do passo 1) é o número que você precisa somar a todos os outros números somados para fazer a validação;
+// 6. O número de cartão de crédito será válido caso o total (do passo 5) seja múltiplo de 10 (ou seja, numero % 10 === 0)
 
 function validateNumber(num) {
   const str = num.toString().split('')
@@ -17,14 +18,16 @@ function validateNumber(num) {
   str.reverse()
   const allEven = []
   for(let i = 0; i < str.length; i++) {
+    let number = Number(str[i])
+
     if(i % 2 == 0) {
-      if(str[i] > 4) {
-      allEven.push(str[i] * 2 - 9)
+      if(number * 2 > 9) {
+      allEven.push(number * 2 - 9)
       } else {
-        allEven.push(str[i] * 2)
+        allEven.push(number * 2)
       }
     } else { 
-      allEven.push(str[i])
+      allEven.push(number)
       }
     }
 
@@ -32,12 +35,20 @@ function validateNumber(num) {
     for(let i = 0; i < allEven.length; i++) {
       soma += allEven[i]
     }
-    return soma + last
+
+    if((soma + last) % 10 === 0) {
+      return `O número de cartão de crédito ${num} é válido`
+    } else {
+      return `O número de cartão de crédito ${num} é inválido` 
+    }
 }
 
 console.log(validateNumber(5555666677778884))
+console.log(validateNumber(5141331902596939))
+console.log(validateNumber(5485755481460022))
+console.log(validateNumber(5261400319746371)) 
+console.log(validateNumber(5381579886310193)) 
 
-// 6. O número de cartão de crédito será válido caso o total (do passo 5) seja múltiplo de 10 (ou seja, numero % 10 === 0)
 
 ///////////////////////
 
@@ -46,18 +57,6 @@ console.log(validateNumber(5555666677778884))
 // Escreva uma função chamada saudarCliente.
 // Essa função deve receber um nome, verificar se ele existe na base de clientes e retornar uma saudação com base em quantas vezes a cliente visitou um estabelecimento. 
 // Consulte o objeto baseClientes abaixo. A saudação deve ser diferente, dependendo do nome da reserva.
-/*
-const baseClientes = {
-  Clotilde: {
-    visitas: 1,
-  },
-  Florinda: {
-    visitas: 2,
-  },
-  Paty: {
-    visitas: 3,
-  },
-}
 
 // Caso 1 - Cliente desconhecida (o nome não está presente no objeto baseClientes)
 // console.log(saudarCliente('Chiquinha')) // --> 'Olá, é a primeira vez por aqui?'
@@ -74,3 +73,35 @@ const baseClientes = {
 // if (nomeCliente === 'Maria') {
 //   // etc
 // }
+
+const baseClientes = {
+  Clotilde: {
+    visitas: 1,
+  },
+  Florinda: {
+    visitas: 2,
+  },
+  Paty: {
+    visitas: 3,
+  },
+}
+
+function saudarCliente(nome) {
+  let cliente = baseClientes[nome]
+
+  if(!cliente) {
+    console.log(`Olá, ${nome}, é a primeira vez por aqui?`)
+  } else {
+  let visitas = cliente.visitas
+
+    if (visitas === 1){
+      console.log(`Bem-vinda, ${nome}! Que bom que voltou!`)
+    } else if (visitas > 1){
+      console.log(`Bem-vinda mais uma vez, ${nome}!`)
+    }
+  }
+}
+
+saudarCliente('Thaísa')
+saudarCliente('Clotilde')
+saudarCliente('Paty')
